2896ea90a0404f9f662d657c208a2f17579a78ab
mkg26 <92150824+MKG26@users.noreply.github.com>
2025-03-19 12:33:34 +0530

    Initial setup and routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { TaskService } from './services/task.service';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { FormsModule } from '@angular/forms';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskListComponent,
+    TaskFormComponent,
+    TaskDetailsComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [TaskService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
